c59c39754877470c5e2030460f00ef224e805e64
jasonthorne <jasonthorne@live.ie>
2021-06-01 17:31:40 +0100

    frame.fxml

--- Workspace/TeamTracker/src/controller/FrameController.java
+++ Workspace/TeamTracker/src/controller/FrameController.java
@@ -1,5 +1,31 @@
 package controller;
 
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
 public class FrameController {
+	
+	//root fxml element & children:
+    @FXML private BorderPane rootBP;
+    @FXML private AnchorPane headerAP;
+    @FXML private Label viewLbl;
+    @FXML private JFXButton notifyBtn;
+    @FXML private StackPane bodySP;
+    @FXML private AnchorPane bodyAP;
+    @FXML private AnchorPane footerAP;
+    @FXML private JFXButton backBtn;
+    @FXML private JFXButton fwrdBtn;
+    
+    @FXML
+    void initialize() {
+        
+    }
+
 
 }
