1bb79004766f22f909551527d3f2aef35d9c6985
jasonthorne <jasonthorne@live.ie>
2022-01-27 20:44:32 +0000

    let and const

---
+++ Section2-JS_Refresher/1_LetAndConst.js
@@ -0,0 +1,8 @@
+
+var myName = "Jay"; // old way of assigning all variables
+
+let myAge = 38; //new way of assigning VARABLES (i.e you want/expect the value to change)
+
+const myHairColour = "Dark Brown" //new way of assigning CONSTANTS (i.e value DOESNT change)
+
+console.log(myName, " ",  myAge, " ", myHairColour);
