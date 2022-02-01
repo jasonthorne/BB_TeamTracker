69771278c7c20fbeea2db4dbe04c55034dceeeea
jasonthorne <jasonthorne@live.ie>
2022-02-01 19:06:57 +0000

    refreshing array functions 1

---
+++ Section2-JS_Refresher/9_RefreshingArrayFunctions.js
@@ -0,0 +1,11 @@
+
+const numArray = [1, 2, 3];
+
+// use build in array method 'map' to double numbers:
+// map does operation to each element in array
+const dblNumArray = numArray.map((element) => {
+    return element*2; //double array element
+});
+
+console.log(dblNumArray); // print array with doubled elements
+
