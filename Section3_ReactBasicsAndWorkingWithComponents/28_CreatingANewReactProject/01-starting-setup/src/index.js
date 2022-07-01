be0afff11ec5ba2d4b02ceb210afaee6aa17e5ae
jasonthorne <jasonthorne@live.ie>
2022-07-01 17:46:00 +0100

    notes

--- Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/index.js
+++ Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import App from './App'; //this is App.js - you omit the .js - only OTHER file types get referenced like.css above 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
