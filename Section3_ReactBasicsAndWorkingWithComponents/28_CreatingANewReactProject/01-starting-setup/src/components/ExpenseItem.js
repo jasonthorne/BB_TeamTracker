d3b97dcb4cbbbcf2c0cfa95d149db83aedc7969a
jasonthorne <jasonthorne@live.ie>
2022-07-06 18:55:16 +0100

    adding basic css styling

--- Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/ExpenseItem.js
+++ Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
 
+
+import './ExpenseItem.css'; //add stylesheet for this component's jsx
+
 /*
 A component is just a function, returning JSX html code 
 */
 
 function ExpenseItem(){ //convention is to repeat the filename
-    return <h2>Im an Expense Item!</h2>;
-}
+    return (
+        <div>
+            <div>Date</div>
+            <div><h2>Title</h2></div>
+            <div>Amount</div>
+        </div>
+    );
+} // NOTE that ONE root html element must be returned (div in this case)
 
 
 //++++++++++++++ REMEMBER - function neeeds exported - THEN imported in another file to use: 
