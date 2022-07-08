53aea6ed454743dc86a2f5856668353d2dbd7987
jasonthorne <jasonthorne@live.ie>
2022-07-08 22:06:40 +0100

    dtnamicx data snd expressions

--- Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/ExpenseItem.js
+++ Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/ExpenseItem.js
@@ -7,13 +7,18 @@ A component is just a function, returning JSX html code
 */
 
 function ExpenseItem(){ //convention is to repeat the filename
+
+    const expenseDate = new Date(2022, 2, 21); //create date with js ++++++ 2 is March as 0 indexed
+    const expenseTitle = 'title here'; //create title
+    const expenseAmount = 333.33; //create title
+
     return (
         <div className='expense-item'> 
-            <div>Date</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Title</h2>
+                <h2>{expenseTitle}</h2> 
             </div>
-            <div className='expense-item__price'>Amount</div>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
     );
 } // NOTE that ONE root html element must be returned (div in this case)
