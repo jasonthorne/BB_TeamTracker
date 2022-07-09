c268da8a5afdf0e77990135adebd8d24c4f7bf2d
jasonthorne <jasonthorne@live.ie>
2022-07-09 23:19:05 +0100

    passing data via props

--- Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/ExpenseItem.js
+++ Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/ExpenseItem.js
@@ -1,28 +1,28 @@
 
-
 import './ExpenseItem.css'; //add stylesheet for this component's jsx
 
 /*
 A component is just a function, returning JSX html code 
 */
 
-function ExpenseItem(){ //convention is to repeat the filename
-
-    const expenseDate = new Date(2022, 2, 21); //create date with js ++++++ 2 is March as 0 indexed
-    const expenseTitle = 'title here'; //create title
-    const expenseAmount = 333.33; //create title
-
+/*react passes ONE param (props) as an arg when passing propoerites to a component +++++++++
+This is an object holding all of the recieved attributes as properties. 
+*/
+function ExpenseItem(props){ //convention is to repeat the filename
+    
     return (
         <div className='expense-item'> 
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2> 
+                <h2>{props.title}</h2>
             </div>
-            <div className='expense-item__price'>${expenseAmount}</div>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
     );
-} // NOTE that ONE root html element must be returned (div in this case)
-// NOTE className instead of class for adding css stling 
+} 
+// NOTE that ONE root html element must be returned (div in this case)
+// NOTE 'className' instead of 'class' for adding css stling 
+// NOTE props.something has to equal the name given to componment attribute in App.js
 
 
 //++++++++++++++ REMEMBER - function neeeds exported - THEN imported in another file to use: 
