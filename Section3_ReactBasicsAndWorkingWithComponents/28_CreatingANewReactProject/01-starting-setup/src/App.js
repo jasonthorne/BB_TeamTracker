18c55d7bb6b2f06bb43fe8e0b2a2438bc65e3a1e
jasonthorne <jasonthorne@live.ie>
2022-07-13 23:42:43 +0100

    assignment 1

--- Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/App.js
+++ Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/App.js
@@ -5,7 +5,8 @@ importing our custom componenet:
 MUST start with capital. lowercase html tags  are read as standar html,
 uppercase are read by react as custom components 
 */
-import ExpenseItem from './components/ExpenseItem'; //+++++++++ NOTE: NO .js extension
+//import ExpenseItem from './components/ExpenseItem'; //+++++++++ NOTE: NO .js extension
+import Expenses from './components/Expenses'; //+++++++++ NOTE: NO .js extension
 
 function App() {
 
@@ -20,10 +21,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started dawg!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
