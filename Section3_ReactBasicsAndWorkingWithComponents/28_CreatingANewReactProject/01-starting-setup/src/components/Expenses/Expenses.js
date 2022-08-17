8e52c970261a2572fceb8ae3f3cc5b4a511375cb
jasonthorne <jasonthorne@live.ie>
2022-08-17 22:56:22 +0100

    lifting the state up

--- Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/Expenses/Expenses.js
+++ Section3_ReactBasicsAndWorkingWithComponents/28_CreatingANewReactProject/01-starting-setup/src/components/Expenses/Expenses.js
@@ -2,10 +2,12 @@
 import './Expenses.css'; //add stylesheet for this component's jsx
 import ExpenseItem from './ExpenseItem'; //import ExpenseDate component
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props){
     return (
         <Card className='expenses'>
+            <ExpensesFilter />
             <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
             <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
             <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
