7df82e447b30e3705e483b61c33b2ebcdccd2110
jasonthorne <jasonthorne@live.ie>
2024-05-29 19:17:26 +0100

    building ticket form

--- learn-react-js/bug-blaster/src/components/TicketForm.js
+++ learn-react-js/bug-blaster/src/components/TicketForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'React';
+import React, {useState} from 'react';
 
 export default function TicketForm(){
     
@@ -13,13 +13,38 @@ export default function TicketForm(){
     const [description, setDescription] = useState('');
     const [priority, setPriority] = useState('1'); //default low priority
 
-    const clearForm = ()=>{
+    const clearForm =()=>{
         setTitle('');
         setDescription('');
         setPriority('1');
     };
 
-    
-    
-    return(<></>);
+    const handleSubmit =(event)=>{
+        //below prevents the entire page from being reloaded on form submit: ++++++++++
+        event.preventDefault(); 
+        clearForm(); //clear form
+    };
+
+    return(
+        <form onSubmit={handleSubmit} className='ticket-form'>
+            <div>
+                <label>Title</label>
+                <input 
+                    type='text' 
+                    value={title}
+                    className='form-input'
+                    onChange={event => setTitle(event.target.value)}
+                ></input>
+            </div>
+            <div>
+                <label>Description</label>
+                <textarea 
+                    type='text' 
+                    value={description}
+                    className='form-input'
+                    onChange={event => setDescription(event.target.value)}
+                ></textarea>
+            </div>
+        </form>
+    );
 };
