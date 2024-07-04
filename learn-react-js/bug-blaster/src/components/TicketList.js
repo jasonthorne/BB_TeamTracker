2df3961b7ccb6c9b16095eeaa6cb6febb1138eda
jasonthorne <jasonthorne@live.ie>
2024-07-04 18:44:07 +0100

    ticket list

---
+++ learn-react-js/bug-blaster/src/components/TicketList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//dispach function needs passed here as it needs to  then be passed (drilled down) to the ticket item component
+export default function TicketList({tickets,dispach}){
+
+    return(
+        <div></div>
+
+    );
+};
