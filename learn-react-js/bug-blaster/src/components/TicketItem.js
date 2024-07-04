184c5fd87218c4f15bc5452b4dfb6e2d62d24e61
jasonthorne <jasonthorne@live.ie>
2024-07-04 18:27:18 +0100

    ticket item

--- learn-react-js/bug-blaster/src/components/TicketItem.js
+++ learn-react-js/bug-blaster/src/components/TicketItem.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-export default function TicketItem(){
+export default function TicketItem({ticket, dispach}){
  
-    
+    //create obj with props. = make it equal to passed ticket:
+    const {id, title, description, priority} = ticket;
 
+    const priorityClass = {
+        1: "priority-low",
+        2: "priority-medium",
+        3: "priority-high"
+    };
+
+    //priority class is whatever number is in ticket.priority gets text priorityClass ++++
+    return (
+        <div className='ticket-item'>
+            <div className={`priority-${priorityClass[ticket.priority]}`}></div>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
+    );
 };
